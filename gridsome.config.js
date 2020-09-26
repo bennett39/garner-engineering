80f11c12e90d31f8a0f9d305ff7b425b46af90ae
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2020-09-26 16:08:40 -0400

    Prettier

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    siteName: 'Garner Engineering',
-    siteDescription: 'Garner Enginnering is a firm in Asheville, NC specializing in dam engineering',
-    siteUrl: 'https://garner-eng.com',
-    titleTemplate: '%s | Garner Engineering',
-    plugins: [],
-}
+  siteName: "Garner Engineering",
+  siteDescription:
+    "Garner Enginnering is a firm in Asheville, NC specializing in dam engineering",
+  siteUrl: "https://garner-eng.com",
+  titleTemplate: "%s | Garner Engineering",
+  plugins: []
+};
